0c113dfaced307b7fcad9f5f392ff5e25f58dce7
Maxime Moreillon <m.moreillon@gmail.com>
2021-09-29 01:20:41 +0000

    simplifications

--- current_consumption.js
+++ current_consumption.js
@@ -9,22 +9,22 @@ const socketio = require('socket.io')
 const dotenv = require('dotenv')
 const pjson = require('./package.json')
 
-dotenv.config();
+dotenv.config()
 
-const DB_name = 'current_consumption'
+const DB_name = process.env.DB_NAME || 'current_consumption'
 const measurement_name = 'current'
+const MQTT_topic = process.env.MQTT_TOPIC || "power/status"
+const LOGGING_PERIOD = process.env.LOGGING_PERIOD || 1 * 60 * 1000
+const port = process.env.APP_PORT || 80
 
-var current_sample = {};
-var last_logging_time = new Date();
+var current_sample = {}
+var last_logging_time = new Date()
 
-const app = express();
-app.use(cors())
+const app = express()
+const http_server = http.Server(app)
+const io = socketio(http_server)
 
-const http_server = http.Server(app);
-const io = socketio(http_server);
 
-var port = 80
-if(process.env.APP_PORT) port=process.env.APP_PORT
 
 // Todo: get URL from env var
 const mqtt_client  = mqtt.connect(process.env.MQTT_URL,{
@@ -35,64 +35,102 @@ const mqtt_client  = mqtt.connect(process.env.MQTT_URL,{
 const influx = new Influx.InfluxDB({
   host: process.env.INFLUXDB_URL,
   database: DB_name,
-  schema: [
-    {
-      measurement: 'current',
-      fields: {
-        phase_1: Influx.FieldType.FLOAT,
-        phase_2: Influx.FieldType.FLOAT,
-        total: Influx.FieldType.FLOAT,
-      },
-      tags: ['unit']
-    }
-  ]
 })
 
 
-const MQTT_topic = "power/status"
-const LOGGING_PERIOD = 2 * 60 * 1000
+
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send({
     application_name: `Current consumption monitoring API`,
     version: pjson.version,
-    influxdb_url: process.env.INFLUXDB_URL || 'undefined',
-    mqtt_url: process.env.MQTT_URL || 'undefined',
+    influxdb: {
+      influxdb_url: process.env.INFLUXDB_URL || 'undefined',
+      db_name: DB_name,
+      measurement_name,
+    },
+
+    mqtt: {
+      broker_url: process.env.MQTT_URL || 'undefined',
+      topic: MQTT_topic,
+    },
+    logging_period: LOGGING_PERIOD,
   })
 })
 
 app.get('/data', (req, res) => {
-  influx.query(`
-    select * from ${measurement_name}
-  `)
+  const query_string = `select * from ${measurement_name}`
+  influx.query(query_string)
   .then( result => res.send(result) )
   .catch( error => res.status(500) );
 })
 
 app.get('/current_consumption', (req, res) => {
-  influx.query(`select * from ${measurement_name} GROUP BY * ORDER BY DESC LIMIT 1`)
+  const query_string = `select * from ${measurement_name} GROUP BY * ORDER BY DESC LIMIT 1`
+  influx.query(query_string)
   .then( result => res.send(result[0]) )
   .catch( error => res.status(500).send(`Error getting current consumption from Influx: ${error}`) );
 })
 
-function drop_database(req, res) {
-  influx.dropDatabase(DB_name)
+
+
+
+const drop_database = async (req, res) => {
+
+  // unused at the moment
+
+  try {
+    await influx.dropDatabase(DB_name)
+    const names = await influx.getDatabaseNames()
+    if (names.includes(DB_name)) throw 'Database was not removed successfully'
+    await influx.createDatabase(DB_name)
+    const query_string = `CREATE RETENTION POLICY "renention_policy" ON "${DB_name}" DURATION 72h REPLICATION 1 DEFAULT`
+    const result = await influx.query(query_string)
+    res.send(result)
+
+  }
+
+  catch (error) {
+    res.status(500).send(error)
+  }
+}
+
+
+const save_point = (current_sample) => {
+  // Log periodically
+  const now = new Date()
+  if( now - last_logging_time < LOGGING_PERIOD) return
+
+  last_logging_time = now
+
+  const points = [
+    {
+      measurement: measurement_name,
+      tags: { unit: "A", },
+      fields: {
+        phase_1: Number(current_sample.phase_1),
+        phase_2: Number(current_sample.phase_2),
+        total: Number(current_sample.phase_1) + Number(current_sample.phase_2),
+      },
+      timestamp: new Date(),
+    }
+  ]
+
+  const parameters = {
+    database: DB_name,
+    precision: 's',
+  }
+
+  influx.writePoints( points, parameters )
   .then( () => {
-    influx.getDatabaseNames()
-    .then(names => {
-      if (!names.includes(DB_name)) {
-        influx.createDatabase(DB_name)
-        .then(() => {
-          influx.query(`CREATE RETENTION POLICY "renention_policy" ON "${DB_name}" DURATION 72h REPLICATION 1 DEFAULT`)
-          .then( result => res.send('OK') )
-          .catch( error =>  res.status(500).send(error) );
-        })
-        .catch( error =>  res.status(500).send(error) );
-      }
-    })
-    .catch(error => res.status(500).send(error));
+    console.log(`[InfluxDB] Measurement saved`)
   })
-  .catch(error => res.status(500).send(error));
+  .catch(error => {
+    console.error(`[InfluxDB] Error saving data to InfluxDB! ${error}`)
+  })
 }
 
 http_server.listen(port, () => console.log(`[Express] Current consumption listening on 0.0.0.0:${port}`))
@@ -101,54 +139,23 @@ http_server.listen(port, () => console.log(`[Express] Current consumption listen
 mqtt_client.on('connect', () => {
   console.log("[MQTT] Connected to MQTT broker")
   mqtt_client.subscribe(MQTT_topic);
-});
+})
+
 
 mqtt_client.on('message', (topic, payload) => {
   console.log(`[MQTT] Message arrived on ${topic}: ${String(payload)}`)
 
+  // Current sample is not very useful asd a global variable
   current_sample = JSON.parse(payload)
 
+  // Update websocket clients
   io.emit('current_consumption',current_sample)
 
+  save_point(current_sample)
 
-  // Log periodically
-  let now = new Date();
-  if( now - last_logging_time > LOGGING_PERIOD) {
-    last_logging_time = now
-    influx.writePoints(
-      [
-        {
-          measurement: measurement_name,
-          tags: {
-            unit: "A",
-          },
-          fields: {
-            phase_1: Number(current_sample.phase_1),
-            phase_2: Number(current_sample.phase_2),
-            total: Number(current_sample.phase_1) + Number(current_sample.phase_2),
-          },
-          timestamp: new Date(),
-        }
-      ], {
-        database: DB_name,
-        precision: 's',
-      })
-      .then( () => {console.log(`[InfluxDB] Measurement saved`)})
-      .catch(error => { console.error(`[InfluxDB] Error saving data to InfluxDB! ${error}`) });
-  }
+})
 
 
-
-
-
-});
-
-
-io.on('connection', (socket) =>{
-  console.log('[WS] a user connected');
-
-
-  socket.on('get_current_consumption', () => {
-    socket.emit('current_consumption', current_sample)
-  })
-});
+io.on('connection', (socket) => {
+  console.log('[WS] a user connected')
+})
